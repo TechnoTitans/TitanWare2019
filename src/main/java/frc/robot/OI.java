2f04fdec767755efac754faf959001ac84538a6b
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2019-01-12 14:24:15 -0500

    Added drive train straight

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,50 +8,60 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.movements.ControlDriveTrain;
+import frc.robot.movements.ControlDriveTrainStraight;
+
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+    private Joystick left, right;
+    //// CREATING BUTTONS
+    // One type of button is a joystick button which is any button on a
+    //// joystick.
+    // You create one by telling it which joystick it's on and which button
+    // number it is.
+    // Joystick stick = new Joystick(port);
+    // Button button = new JoystickButton(stick, buttonNumber);
 
-  private Joystick left = new Joystick(RobotMap.LEFT_JOYSTICK);
-  private Joystick right = new Joystick(RobotMap.RIGHT_JOYSTICK);
+    // There are a few additional built in buttons you can use. Additionally,
+    // by subclassing Button you can create custom triggers and bind those to
+    // commands the same as any other Button.
 
-	public double getLeft() {
-		return -left.getY();
-  }
-  
-  public double getRight() {
-    return -right.getY();
-  }
+    //// TRIGGERING COMMANDS WITH BUTTONS
+    // Once you have a button, it's trivial to bind it to a button in one of
+    // three ways:
 
-  
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+    // Start the command when the button is pressed and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenPressed(new ExampleCommand());
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+    // Run the command while the button is being held down and interrupt it once
+    // the button is released.
+    // button.whileHeld(new ExampleCommand());
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+    // Start the command when the button is released and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenReleased(new ExampleCommand());
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+    public void initialize() {
+        left = new Joystick(RobotMap.LEFT_JOYSTICK);
+        right = new Joystick(RobotMap.RIGHT_JOYSTICK);
+        Button driveTriggerLeft = new JoystickButton(left, 0);
+        driveTriggerLeft.whenPressed(new ControlDriveTrainStraight(this));
+        driveTriggerLeft.whenReleased(new ControlDriveTrain(this));
+    }
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+    public double getLeft() {
+        return -left.getY();
+    }
+
+    public double getRight() {
+        return -right.getY();
+    }
 }
