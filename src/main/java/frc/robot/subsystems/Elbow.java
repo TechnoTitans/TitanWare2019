4bb42ab51aa2c9428e4e692b01c9e23b353c6a4b
William <WolfEpic221@gmail.com>
2019-01-12 11:48:02 -0500

    Elbow

--- src/main/java/frc/robot/subsystems/Elbow.java
+++ src/main/java/frc/robot/subsystems/Elbow.java
@@ -8,11 +8,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Elbow extends Subsystem {
   private TalonSRX elbowTalon;
 
-  public Elbow(TalonSRX)
+  public Elbow(TalonSRX elbowTalon) {
+    this.elbowTalon = elbowTalon;
+  }
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
     //IDK mate
+  }
+
+  public void moveUp(double speed){
+    if(speed > 0.01 || speed < -0.01){
+      elbowTalon.set(speed);
+    } else {
+      elbowTalon.set(0);
+    }
   }
 }
