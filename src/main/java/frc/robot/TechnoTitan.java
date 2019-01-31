824fa07669e1a559ee74ee4255533cd83ed3615e
Yaseen Shaikh <yaseen.nvt@gmail.com>
2019-01-31 16:20:22 -0500

    Fix bug with packaging issue lmao

--- src/main/java/frc/robot/TechnoTitan.java
+++ src/main/java/frc/robot/TechnoTitan.java
@@ -8,25 +8,19 @@
 package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.hal.util.UncleanStatusException;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.filters.LinearDigitalFilter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.motor.TalonSRX;
-import frc.robot.sensors.lis3dh.Accel_LIS3DH;
 import frc.robot.sensors.Accel_GY521;
 import frc.robot.sensors.QuadEncoder;
 import frc.robot.sensors.TimeOfFlight;
 import frc.robot.sensors.VisionSensor;
+import frc.robot.sensors.lis3dh.Accel_LIS3DH;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.I2C;
 import frc.robot.subsystems.TankDrive;
 
 
--- src/main/java/frc/robot/sensors/lis3dh/Accel_LIS3DH.java
+++ src/main/java/frc/robot/sensors/lis3dh/Accel_LIS3DH.java
@@ -1,4 +1,4 @@
-package frc.robot.sensors;
+package frc.robot.sensors.lis3dh;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.PIDSource;
