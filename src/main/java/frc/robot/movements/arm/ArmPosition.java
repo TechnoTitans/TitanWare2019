2bae2a651e9193ce8505fd699d30bce883e84d06
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2019-02-08 20:46:51 -0500

    Added high arm positions

--- src/main/java/frc/robot/movements/arm/ArmPosition.java
+++ src/main/java/frc/robot/movements/arm/ArmPosition.java
@@ -4,11 +4,12 @@ package frc.robot.movements.arm;
 import frc.robot.subsystems.Arm;
 
 public enum ArmPosition {
+    // TODO: refine these
     ROCKET_LEVEL_1_BALL(-10.3, 0, false),
     ROCKET_LEVEL_2_BALL(42.2, 0, false),
-    ROCKET_LEVEL_3_BALL(90, 90, true), // TODO: we need to know solenoid height for this
+    ROCKET_LEVEL_3_BALL(51, 28.8, true),
     ROCKET_LEVEL_2_HATCH(8.6, 90, false),
-    ROCKET_LEVEL_3_HATCH(90,90,true), // TODO: we need to know solenoid height
+    ROCKET_LEVEL_3_HATCH(33.6,90,true),
     CARGO_SHIP_BALL(-28, 0, false),
     LOW_HATCH(-40, 90, false), // All hatch levels except higher hatches on rocket
     BALL_PICKUP(-35.8, -20.6, false);
