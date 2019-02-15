5a77ab3298b28ae32cfcbc094d64fe6fa5d0e888
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2019-02-14 19:38:36 -0500

    Continue working on arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -128,7 +128,7 @@ public class Arm extends Subsystem {
 
         elbowController = new PIDAngleController("Elbow", kElbowP, kElbowI, kElbowD, MAX_STEADY_VOLTAGE_ELBOW, elbowAngleSensor, this::moveElbow, 0.035);
         wristController = new PIDAngleController("Wrist", kWristP, kWristI, kWristD, MAX_STEADY_VOLTAGE_WRIST, wristAngleSensor, this::moveWrist, 0.02);
-        elbowController.setOutputRange(0.05, 0.6);
+        elbowController.setOutputRange(0.05, 0.5);
         wristController.setOutputRange(-0.5, 0.5);
 
         SmartDashboard.putData("Elbow", elbowController);
