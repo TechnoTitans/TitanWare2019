59324165c6e0a1fe5826fae5a6fe3ba368b9728c
Yaseen Shaikh <yaseen.nvt@gmail.com>
2019-02-20 22:02:15 -0500

    Make decent tests for Accel GY521

--- src/test/java/org/technotitans/sensor/AccelerometerTester.java
+++ src/test/java/org/technotitans/sensor/AccelerometerTester.java
@@ -1,23 +1,51 @@
-//package org.technotitans.sensor;
-//
-//import edu.wpi.first.wpilibj.I2C;
-//import org.junit.Test;
-//
-//
-//import static org.junit.Assert.assertTrue;
-//
-//public class AccelerometerTester {
-//
-//    @Test
-//    public void testElbowSensor() {
-//        I2C elbowI2C_conn = new I2C(I2C.Port.kOnboard, RobotMap.ELBOW_ACCEL_ADDR);
-//        assertTrue(elbowI2C_conn.verifySensor(REG_WHO_AM_I, 1, WHOAMI_DEFAULT_VALUE));
-//    }
-//
-//    @Test
-//    public void testWristSensor() {
-//        I2C wristI2C_conn = new I2C(I2C.Port.kOnboard, RobotMap.WRIST_ACCEL_ADDR);
-//        assertTrue(wristI2C_conn.verifySensor(REG_WHO_AM_I, 1, WHOAMI_DEFAULT_VALUE)); // TODO correctly implement the second sensor
-//    }
-//
-//}
+package org.technotitans.sensor;
+
+import frc.robot.sensors.gy521.Accel_GY521;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class AccelerometerTester {
+
+    private static final double EPSILON = 1e-6;
+    private List<Accel_GY521> sensors;
+
+    @Before
+    public void setup() {
+        Accel_GY521 elbow = mock(Accel_GY521.class);
+        Accel_GY521 wrist = mock(Accel_GY521.class);
+        this.sensors.add(elbow);
+        this.sensors.add(wrist);
+    }
+
+    @Test
+    public void sensorsShouldBeConnected() {
+        sensors.forEach(s -> {
+            assertTrue(s.isSensorConnected());
+        });
+    }
+
+    @Test
+    public void sensorsShouldOutputReadingsOnAccelerometer() {
+        // TODO Determine if this is a good way to test or not
+        // fails if the readings are exactly 0.0
+        sensors.forEach(s -> {
+            assertNotEquals(0.0, s.getX());
+            assertNotEquals(0.0, s.getY());
+            assertNotEquals(0.0, s.getZ());
+        });
+    }
+
+    @Test
+    public void sensorsGiveValidAngleAfterCalculations() {
+        sensors.forEach(s -> {
+            double angle = s.getAngle();
+            assertTrue(0.0 < angle && angle < 90.0);
+        });
+    }
+}
