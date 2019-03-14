67acf0934d1f99c4173b05c05a25508d0ffce3ea
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2019-03-14 19:05:59 -0400

    Removed backup from ReleaseHatch

--- src/main/java/frc/robot/movements/ReleaseHatch.java
+++ src/main/java/frc/robot/movements/ReleaseHatch.java
@@ -8,22 +8,18 @@ import frc.robot.TechnoTitan;
 public class ReleaseHatch extends CommandGroup {
     public ReleaseHatch() {
         requires(TechnoTitan.grabber);
-        requires(TechnoTitan.drive);
         addSequential(new InstantCommand(() -> {
             TechnoTitan.grabber.setClawPistons(false); // make sure that they are in
             TechnoTitan.grabber.setPancakePistons(true);
-            TechnoTitan.drive.set(-0.3);
         }));
         addSequential(new WaitCommand(0.25));
         addSequential(new InstantCommand(() -> {
             TechnoTitan.grabber.setPancakePistons(false);
-            TechnoTitan.drive.set(0);
         }));
     }
 
     @Override
     public void interrupted() {
         TechnoTitan.grabber.setPancakePistons(false);
-        TechnoTitan.drive.set(0);
     }
 }
