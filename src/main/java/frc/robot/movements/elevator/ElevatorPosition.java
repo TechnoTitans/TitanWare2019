170fd208a33d222887f8da3b68699ab8126cf9c7
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2019-03-28 16:15:38 -0400

    Fix encoder bug by changing scale factor to measured value Small changes with elevator positions

--- src/main/java/frc/robot/movements/elevator/ElevatorPosition.java
+++ src/main/java/frc/robot/movements/elevator/ElevatorPosition.java
@@ -5,7 +5,7 @@ public enum ElevatorPosition {
     ROCKET_LEVEL_2_BALL(-70, 31),
     ROCKET_LEVEL_3_BALL(-70, 59),
     CARGO_SHIP_BALL(-70, 21),
-    BALL_PICKUP(2.6, 0),
+    BALL_PICKUP(10, 0),
     LOW_HATCH(-90, 0.6),
     ROCKET_LEVEL_2_HATCH(-90, 31),
     ROCKET_LEVEL_3_HATCH(-90, 60.8),
